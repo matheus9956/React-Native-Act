a1c74a8a6f58389a880e1b531afdc12dc0c0decd
Gabriel Camboim <gabrielcamboim99@gmail.com>
2020-11-27 13:33:51 -0300

    Editado tab bar

--- App.js
+++ App.js
@@ -11,13 +11,61 @@ import GroupScreen from "./src/screens/GroupScreen";
 import { Provider } from "./src/context/FamilyContext";
 import React from "react";
 import FormScreen from "./src/screens/FormScreen";
+import Ionicons from "react-native-vector-icons/Ionicons";
 
-const TabNavigator = createBottomTabNavigator({
-  Register: RegisterScreen,
-  Families: FamiliesScreen,
-  Groups: GroupsScreen,
-  Settings: SettingsScreen,
-});
+const TabNavigator = createBottomTabNavigator(
+  {
+    Register: {
+      screen: RegisterScreen,
+      navigationOptions: () => ({
+        tabBarLabel: "Cadastrar",
+      }),
+    },
+    Families: {
+      screen: FamiliesScreen,
+      navigationOptions: () => ({
+        tabBarLabel: "Famílias",
+      }),
+    },
+    Groups: {
+      screen: GroupsScreen,
+      navigationOptions: () => ({
+        tabBarLabel: "Grupos",
+      }),
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: () => ({
+        tabBarLabel: "Ajustes",
+      }),
+    },
+  },
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === "Register") {
+          iconName = focused ? "ios-list-box" : "ios-list";
+        } else if (routeName === "Settings") {
+          iconName = focused ? "ios-list-box" : "ios-list";
+        } else if (routeName === "Families") {
+          iconName = focused ? "ios-list-box" : "ios-list";
+        } else if (routeName === "Groups") {
+          iconName = focused ? "ios-list-box" : "ios-list";
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      },
+    }),
+    tabBarOptions: {
+      activeTintColor: "#0080ff",
+      inactiveTintColor: "gray",
+    },
+  }
+);
 
 const navigator = createStackNavigator(
   {
